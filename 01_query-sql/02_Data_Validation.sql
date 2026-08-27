927693b67aa61ce8e97a8417ca3c5b5851b2448a
Lautaro Vila Gallardo <vgallardolautaro@gmail.com>
2026-08-27 09:39:56 -0300

    Update 02_Data_Validation.sql

--- 01_query-sql/02_Data_Validation.sql
+++ 01_query-sql/02_Data_Validation.sql
@@ -1,1 +1,25 @@
 
+SELECT *
+FROM hr_employee_data
+WHERE 
+    employeenumber IS NULL
+    OR age IS NULL
+    OR department IS NULL
+    OR jobrole IS NULL
+    OR monthlyincome IS NULL;
+
+--------
+UPDATE hr_employee_data
+SET
+    employeenumber = COALESCE(employeenumber, 0),
+    age = COALESCE(age, 0),
+    monthlyincome = COALESCE(monthlyincome, 0),
+    department = COALESCE(department, 'NULO'),
+    jobrole = COALESCE(jobrole, 'NULO')
+WHERE
+    employeenumber IS NULL
+    OR age IS NULL
+    OR department IS NULL
+    OR jobrole IS NULL
+    OR monthlyincome IS NULL;
+--------
