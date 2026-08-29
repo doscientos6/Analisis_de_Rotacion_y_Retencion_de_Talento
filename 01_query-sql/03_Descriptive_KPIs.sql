759969cc4d81fcaaab626968f8aee40407f7ebd3
Lautaro Vila Gallardo <vgallardolautaro@gmail.com>
2026-08-29 18:53:33 -0300

    Update 03_Descriptive_KPIs.sql

--- 01_query-sql/03_Descriptive_KPIs.sql
+++ 01_query-sql/03_Descriptive_KPIs.sql
@@ -2,13 +2,19 @@
 
 --A. CANTIDAD TOTAL DE EMPLEADOS
 SELECT COUNT(*) AS total_empleados 
-FROM 
+FROM hr_employee_data
 
 --B. CANTIDAD DE EMPLEADOS QUE DESERTARON 
 SELECT COUNT(*) AS empleados_desertados
-FROM 
+FROM hr_employee_data
 WHERE Attrition = 'Yes'
 
 --C. DISTRIBUCIÓN DE GÉNERO
+SELECT gender, COUNT(*) 
+FROM hr_employee_data
+GROUP BY gender
 
 --D. DEPARTAMENTOS DE LA EMPRESA
+SELECT department
+FROM hr_employee_data
+GROUP BY deparment
