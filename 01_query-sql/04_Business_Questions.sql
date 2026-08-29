8edeec377206b4ed1361f38ee874b3dcb234f93b
Lautaro Vila Gallardo <vgallardolautaro@gmail.com>
2026-08-29 19:14:53 -0300

    Update 04_Business_Questions.sql

--- 01_query-sql/04_Business_Questions.sql
+++ 01_query-sql/04_Business_Questions.sql
@@ -1,7 +1,14 @@
 --4. REALIZAMOS PREGUNTAS QUE PUEDAN AYUDAR A LA EMPRESA A ENTENDER POR QUE LA POSIBLE ALTA TASA DE ATTRITION.
 
---A. 
+--A. Salario promedio 
 
---B.
+--B. Distancia promedio desde el hogar hasta la zona de trabajo
 
+--C. Satisfacción con entorno laboral
+
+--D. Realización de horas extras
+
+--E. Satisfacción en las relaciones
+
+--F. Edad promedio 
 
