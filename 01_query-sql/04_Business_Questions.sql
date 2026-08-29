a5521738f166cf7d83fe211ddca817fe5e80ff58
Lautaro Vila Gallardo <vgallardolautaro@gmail.com>
2026-08-29 19:48:00 -0300

    Update 04_Business_Questions.sql

--- 01_query-sql/04_Business_Questions.sql
+++ 01_query-sql/04_Business_Questions.sql
@@ -10,6 +10,7 @@ ORDER BY attrition, deparment;
 SELECT attrition, ROUND(AVG(distancefromhome), 1) AS distancia_promedio
 FROM empleados
 GROUP BY attrition;
+
 --C. SATISFACCIÓN CON ENTORNO LABORAL
 SELECT attrition, ROUND(AVG(jobsatisfaction), 2) AS satisfaccion_trabajo_promedio
 FROM empleados
